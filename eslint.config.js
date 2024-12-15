204e4f694d13f279da53973145ab447c39a3ce7c
Markobanduka <banduka44@gmail.com>
2024-12-15 23:58:38 +0100

    images

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from "globals";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import reactThreeFiber from "eslint-plugin-react-three-fiber";
 
 export default [
   { ignores: ["dist"] },
@@ -17,17 +18,21 @@ export default [
         sourceType: "module",
       },
     },
-    settings: { react: { version: "18.3" } },
+    settings: {
+      react: { version: "18.3" },
+    },
     plugins: {
       react,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
+      "react-three-fiber": reactThreeFiber,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs["jsx-runtime"].rules,
       ...reactHooks.configs.recommended.rules,
+      ...reactThreeFiber.configs.recommended.rules,
       "no-unused-vars": "warn",
       "react/prop-types": "off",
       "react/jsx-no-target-blank": "off",
