b256bd0b9b501c6dfb209e181fb276499f7c7ec6
Markobanduka <banduka44@gmail.com>
2025-01-01 21:39:39 +0100

    remove unused class from HeroOrbit component

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -8,7 +8,6 @@ const HeroOrbit = ({
   return (
     <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
       <div
-        className=""
         style={{
           transform: `rotate(${rotation}deg)`,
           height: `${size}px`,
