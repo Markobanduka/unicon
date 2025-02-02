998f2b2a0b259ec895ae5b5c8a1e1de70197b61a
Markobanduka <banduka44@gmail.com>
2025-02-02 12:28:40 +0100

    refactor ToolboxItems component to remove unused itemsWrapperClassName prop and simplify layout

--- src/components/Tool2.tsx
+++ src/components/Tool2.tsx
@@ -5,7 +5,6 @@ import { Fragment } from "react";
 const ToolboxItems = ({
   items,
   className,
-  itemsWrapperClassName,
 }: {
   items: { title: string; iconType: React.ElementType }[];
   className?: string;
@@ -18,28 +17,21 @@ const ToolboxItems = ({
         className
       )}
     >
-      <div
-        className={twMerge(
-          "flex flex-none py-0.5 gap-6 pr-6",
-          itemsWrapperClassName
-        )}
-      >
-        {[
-          ...new Array(2).fill(2).map((_, index) => (
-            <Fragment key={index}>
-              {items.map((item) => (
-                <div
-                  key={item.title}
-                  className="inline-flex items-center gap-4 py-2 px-3 outline outline-2 outline-white/10 rounded-lg"
-                >
-                  <TechIcon component={item.iconType} />
-                  <span className="font-semibold">{item.title}</span>
-                </div>
-              ))}
-            </Fragment>
-          )),
-        ]}
-      </div>
+      {[
+        ...new Array(2).fill(2).map((_, index) => (
+          <Fragment key={index}>
+            {items.map((item) => (
+              <div
+                key={item.title}
+                className="inline-flex items-center gap-4 py-2 px-3 outline outline-2 outline-white/10 rounded-lg"
+              >
+                <TechIcon component={item.iconType} />
+                <span className="font-semibold">{item.title}</span>
+              </div>
+            ))}
+          </Fragment>
+        )),
+      ]}
     </div>
   );
 };
