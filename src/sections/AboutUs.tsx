ccf6994eb1c5d9776cd3905f761b3032f3c7f417
Markobanduka <banduka44@gmail.com>
2025-02-24 02:14:02 +0100

    add new images and enhance content in About Us section

--- src/sections/AboutUs.tsx
+++ src/sections/AboutUs.tsx
@@ -3,6 +3,9 @@ import Image from "next/image";
 import Picture1 from "@/assets/images/Picture1.jpg";
 import Picture2 from "@/assets/images/Picture2.jpg";
 import Picture3 from "@/assets/images/Picture3.jpg";
+import Picture4 from "@/assets/images/Picture4.jpg";
+import Picture5 from "@/assets/images/Picture5.jpg";
+import Picture6 from "@/assets/images/Picture6.jpg";
 
 const AboutUsSection = () => {
   return (
@@ -101,27 +104,65 @@ const AboutUsSection = () => {
             <div className="py-6">
               <h3 className="italic underline">Staff Resource</h3>
               <p className="py-4">
-                Unicon&apos;s strength among others, include a clear corporate
-                structure and planning, multilingual and well qualified
-                permanent staff of Managers, Co-ordinators, Contract and Project
-                Managers, Site Managers, Electricians and efficient back office
-                staff constituting, Financial Managers, Procurement Managers to
-                mention but a few. The cosmopolitan nature of our work force at
-                all levels our organization enables us to fulfill our aim of
-                providing a high level of client service and satisfaction. Our
-                Personnel are versatile to all kinds of working environments and
-                standards. Frequent training sessions conducted by management in
-                all aspects of our operations and technology updates, ads up to
-                our staff&apos;s performance in their respective fields. Some of
-                the aspects of training include quality control, health &
-                safety, first aid, management skills and updates on latest
-                technology and equipment to ensure excellent efficiency and
-                expertise enabling them to work in close collaboration with
-                consultants, architects and clients.
+                Unicon&apos;s strengths include a clear corporate structure and
+                planning, along with a multilingual and highly qualified team.
+                Our permanent staff consists of Managers, Coordinators, Contract
+                and Project Managers, Site Managers, Electricians, and an
+                efficient back-office team, including Financial and Procurement
+                Managers, among others. The cosmopolitan nature of our work
+                force at all levels our organization enables us to fulfill our
+                aim of providing a high level of client service and
+                satisfaction. Our Personnel are versatile to all kinds of
+                working environments and standards. Frequent training sessions
+                conducted by management in all aspects of our operations and
+                technology updates, ads up to our staff&apos;s performance in
+                their respective fields. Some of the aspects of training include
+                quality control, health & safety, first aid, management skills
+                and updates on latest technology and equipment to ensure
+                excellent efficiency and expertise enabling them to work in
+                close collaboration with consultants, architects and clients.
               </p>
-              <div className="flex justify-center">
+              <div className="flex justify-center ">
                 <Image src={Picture3} alt="Image of our stuff" />
               </div>
+            </div>
+            <div className="py-6 mb-10">
+              <h3 className="italic underline">Procurement</h3>
+              <div className="py-6">
+                <p className="mb-2">
+                  In comparison to our craftsmanship, the use of superior
+                  materials is second to none. This is selectively done by our
+                  procurement team which maintains a database of market contacts
+                  and vigorously surveys all markets for the highest quality and
+                  supplies to be sourced.
+                </p>
+
+                <div className="md:float-left md:mr-4 md:mb-2 w-48 md:w-64 mx-auto md:mx-0 mt-4 md:mt-0">
+                  <Image
+                    src={Picture4}
+                    alt="Image of our project"
+                    className="w-full h-auto"
+                  />
+                </div>
+
+                <p className="mt-4 md:mt-0">
+                  The materials we use are sourced both locally and abroad for
+                  maximum choice. We keep databases for all the materials we
+                  buy, for future cost comparison so that we are able to meet
+                  our client&apos;s needs and budget. The economic benefits in
+                  this department by the maintenance of high standards and
+                  procedures guarantee our client optimum competitive rates for
+                  materials and equipment.
+                </p>
+              </div>
+              <div className="flex md:flex-row flex-col gap-4 md:gap-0 items-center">
+                <Image src={Picture5} alt="Image of our materials" />
+                <Image src={Picture6} alt="Image of our materials" />
+              </div>
+            </div>
+            <div className="py-6">
+              <h3 className="italic underline">Quality Assurance</h3>
+              <p className="py-4"></p>
             </div>
           </div>
         </div>
